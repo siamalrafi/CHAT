b7e912b9fdb34b215aa5b27cb5fab363f598b36f
siamalrafi <rahathasandipto@gmail.com>
2023-05-25 17:52:37 +0600

    app is running with chat data

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,22 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const { chats } = require("./data/data");
+
 app.get("/", (req, res) => {
-   res.send("Hello World!");
+   res.send("Chatting server is running.");
+});
+
+// chats data get request ---
+app.get("/api/chat", (req, res) => {
+   res.send(chats);
+});
+
+// get chat data by id ---
+app.get("/api/chat/:id", (req, res) => {
+   const id = req.params.id;
+   const chatData = chats.find((chatData) => chatData._id === id);
+   res.send(chatData);
 });
 
 app.listen(port, () => {
