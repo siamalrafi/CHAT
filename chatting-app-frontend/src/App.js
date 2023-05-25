8b8df88339e8e8db8628bd416f21a72784c52c6b
siamalrafi <rahathasandipto@gmail.com>
2023-05-25 18:28:42 +0600

    this is the frontend set up

---
+++ chatting-app-frontend/src/App.js
@@ -0,0 +1,15 @@
+import { Button } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+   return (
+      <div className="App">
+         https
+         <Button colorScheme="teal" size="xs">
+            Button
+         </Button>
+      </div>
+   );
+}
+
+export default App;
